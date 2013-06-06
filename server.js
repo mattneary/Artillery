16178c4f9c5d0755ce6949e2d996633ba2678106
Matt Neary <neary.matt@gmail.com>
2013-06-05 21:01:27 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+	fs = require('fs');
+	
+exports.module = http.createServer(function(req, res) {	
+	var path = __dirname + (req.url == '/' ? '/index.html' : req.url);
+	fs.stat(path, function(err, stat) {
+	    if (!err) {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			fs.createReadStream(path).pipe(res);
+	    }else {
+	        res.writeHead(404);
+	        res.end();
+	    }
+	});
+}).listen(8080);
