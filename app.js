5065587ecceb0d47c5fd3a7de39dd7bae174d185
Matt Neary <neary.matt@gmail.com>
2013-06-07 20:32:21 -0400

    code clean up, classes

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 var cvs = document.getElementById('cvs'),
 	ctx = cvs.getContext('2d');				
 	
-var dx1 = [], dy1 = [],
-	dx2 = [], dy2 = [],
-	x1 = 225, y1 = 250,
-	x2 = 275, y2 = 250;
-	
 // Parser		
 var cases = function(str) {
 	return str.match(/\s*?else\s*?/g) ? 'true' : str;
@@ -52,26 +47,63 @@ $("#deploy").on("click", function(evt) {
 	x2 = 275, y2 = 250;
 });
 	
-var add = function(a,b){return a+b};
+// Helper functions	
 var magnitude = function(a,b){return Math.sqrt(a*a + b*b)};
-// Animation Loop
-var animate = function() {
-	var pad = navigator.webkitGetGamepads()[0];
-	dx1.push(pad.axes[0]);
-	dy1.push(pad.axes[1]);
-	dx2.push(pad.axes[2]);
-	dy2.push(pad.axes[3]);			
+
+// Classes
+var Wheel = function(x, y) {
+	this.x = x;
+	this.y = y;	
+};
+var Tank = function(controller, cvs) {
+	var ctx = cvs.getContext('2d');
+	this.wheels = [
+		new Wheel(225, 250), 
+		new Wheel(275, 250), 
+		new Wheel(225, 300), 
+		new Wheel(275, 300)
+	];
+	this.render = function() {
+		cvs.width = cvs.width;
+		var a = this.wheels[0],
+			b = this.wheels[1],
+			c = this.wheels[2],
+			d = this.wheels[3];
 	
-	cvs.width = cvs.width;
-	
-	var orientation = Math.PI/2;
-	if( dx1.length > 2 && dx2.length > 2 ) {
-		var y1joy = dy1.slice(-2).reduce(add) / 2 * 20;
-		var y2joy = dy2.slice(-2).reduce(add) / 2 * 20;
-		var x1joy = dx1.slice(-2).reduce(add) / 2 * 20;
-		var x2joy = dx2.slice(-2).reduce(add) / 2 * 20;
-		var leftSpeed = leftPower(y1joy, y2joy, x1joy, x2joy);
-		var rightSpeed = rightPower(y1joy, y2joy, x1joy, x2joy);
+		// Leading wheels		
+		ctx.beginPath();
+			ctx.arc(b.x, b.y, 10, 0, Math.PI*2);
+			ctx.arc(a.x, a.y, 10, 0, Math.PI*2);
+			ctx.fillStyle = '#f00';
+			ctx.fill();	
+		ctx.closePath();
+		
+		// Back wheels
+		ctx.beginPath();			
+			ctx.arc(d.x, d.y, 10, 0, Math.PI*2);
+			ctx.arc(c.x, c.y, 10, 0, Math.PI*2);
+			ctx.fillStyle = '#000';
+			ctx.fill();	
+		ctx.closePath();
+		
+		// Frame
+		ctx.beginPath();
+			ctx.moveTo(a.x, a.y);	
+			ctx.lineTo(b.x, b.y);
+			ctx.lineTo(c.x, c.y);	
+			ctx.lineTo(d.x, d.y);	
+			ctx.lineTo(a.x, a.y);							
+			ctx.stroke();							
+		ctx.closePath();
+	};
+	this.animate = function() {
+		// Establish two side speeds based on user power functions.
+		controller.update();
+		var leftSpeed = leftPower(controller.y1, controller.y2, controller.x1, controller.x2);
+		var rightSpeed = rightPower(controller.y1, controller.y2, controller.x1, controller.x2);
+		
+		// Find velocity shared by both wheels, thus the velocity exhibited...
+		// ... by the tank as a whole.
 		var shared;
 		if( Math.abs(leftSpeed) > Math.abs(rightSpeed) ) {
 			shared = rightSpeed;
@@ -79,71 +111,79 @@ var animate = function() {
 			shared = leftSpeed;
 		}
 		
-		var rotation = Math.PI/8 * (rightSpeed - leftSpeed)/20;
-		var angle = Math.atan((y1-y2)/(x1-x2));		
-		if( x1-x2 < 0 ) {
-			angle = Math.PI + angle;
-		}					
-		
-		orientation = Math.PI/2 - angle;					
-		var separation = magnitude(y1-y2, x1-x2);		
-		if( rotation < 0 ) {
-			deltaX = separation*Math.cos(angle-rotation);
-			deltaY = separation*Math.sin(angle-rotation);
-			y2 = y1 - deltaY;
-			x2 = x1 - deltaX;		
-		} else if( rotation > 0 ) {
-			deltaX = separation*Math.cos(angle-rotation);
-			deltaY = separation*Math.sin(angle-rotation);
-			y1 = y2 + deltaY;
-			x1 = x2 + deltaX;		
-		}
-		
-		y1 += shared * Math.sin(orientation);
-		y2 += shared * Math.sin(orientation);
-		x1 -= shared * Math.cos(orientation);
-		x2 -= shared * Math.cos(orientation);
-		
-		angle = Math.atan((y1-y2)/(x1-x2));		
-		if( x1-x2 < 0 ) {
+		// Determine vector between the front two wheels. 
+		var separation = magnitude(this.wheels[0].y-this.wheels[1].y, this.wheels[0].x-this.wheels[1].x),
+			angle = Math.atan((this.wheels[0].y-this.wheels[1].y)/(this.wheels[0].x-this.wheels[1].x));;
+		if( this.wheels[0].x-this.wheels[1].x < 0 ) {
 			angle = Math.PI + angle;
 		}	
-		orientation = Math.PI/2 - angle;		
-	}
-					
-	ctx.beginPath();
-	var backShift = Math.sin(orientation) * 50;
-	var sideShift = Math.cos(orientation) * 50;
-			
-	// Leading wheels		
-	ctx.arc(x2, y2, 10, 0, Math.PI*2);
-	ctx.arc(x1, y1, 10, 0, Math.PI*2);
-	ctx.fillStyle = '#f00';
-	ctx.fill();	
-	ctx.closePath();
-	
-	// Back wheels
-	ctx.beginPath();
-	ctx.fillStyle = '#000';
-	ctx.arc(x2-sideShift, y2+backShift, 10, 0, Math.PI*2);
-	ctx.arc(x1-sideShift, y1+backShift, 10, 0, Math.PI*2);
-	ctx.fill();	
-	ctx.closePath();
-	
-	// Frame
-	ctx.beginPath();
-	ctx.moveTo(x1, y1);	
-	ctx.lineTo(x1-sideShift, y1+backShift);
-	ctx.lineTo(x2-sideShift, y2+backShift);	
-	ctx.lineTo(x2, y2);	
-	ctx.lineTo(x1, y1);							
-	ctx.stroke();							
-	ctx.closePath();	
+		
+		// Convert wheel power discrepancy to rotation action...
+		// ... then perform this with the slower wheel as a pivot.
+		var rotation = Math.PI/8 * (rightSpeed - leftSpeed)/20,
+			deltaX = separation*Math.cos(angle-rotation),
+			deltaY = separation*Math.sin(angle-rotation);
+		if( rotation < 0 ) {
+			this.wheels[1].y = this.wheels[0].y - deltaY;
+			this.wheels[1].x = this.wheels[0].x - deltaX;		
+		} else if( rotation > 0 ) {
+			this.wheels[0].y = this.wheels[1].y + deltaY;
+			this.wheels[0].x = this.wheels[1].x + deltaX;
+		}	
+		
+		// Move entire tank with the shared velocity, where orientation...
+		// ... is the direction in which movement will occur.
+		var orientation = Math.PI/2 - angle;
+		this.wheels[0].y += shared * Math.sin(orientation);
+		this.wheels[1].y += shared * Math.sin(orientation);
+		this.wheels[0].x -= shared * Math.cos(orientation);
+		this.wheels[1].x -= shared * Math.cos(orientation);
+		
+		// Find new angle between the front wheels...
+		angle = Math.atan((this.wheels[0].y-this.wheels[1].y)/(this.wheels[0].x-this.wheels[1].x));
+		if( this.wheels[0].x-this.wheels[1].x < 0 ) {
+			angle = Math.PI + angle;
+		}	
+		// ... then use it to recalculate orientation.
+		orientation = Math.PI/2 - angle;
+		
+		// Adjust back wheels to reflect changed orientation
+		var backShift = Math.sin(orientation) * 50,
+			sideShift = Math.cos(orientation) * 50;
+		this.wheels[2] = new Wheel(this.wheels[0].x-sideShift, this.wheels[0].y+backShift);
+		this.wheels[3] = new Wheel(this.wheels[1].x-sideShift, this.wheels[1].y+backShift);
+		
+		// Render self in canvas
+		this.render();
+		
+		// Animate the next frame
+		window.requestAnimationFrame(function() { this.animate(); }.bind(this));
+	};	
 };
+var Controller = function() {
+	var pad = navigator.webkitGetGamepads()[0];
+	this.x1 = pad.axes[0];
+	this.y1 = pad.axes[1];
+	this.x2 = pad.axes[2];
+	this.y2 = pad.axes[3];
+	this.update = function() {
+		pad = navigator.webkitGetGamepads()[0];
+		this.x1 = pad.axes[0] * 5;
+		this.y1 = pad.axes[1] * 5;
+		this.x2 = pad.axes[2] * 5;
+		this.y2 = pad.axes[3] * 5;
+	};
+};
+
 // Wait for Gamepad
+var tank, controller;
 var poll = function() {
 	if(navigator.webkitGetGamepads()[0]) {
-		setInterval(animate, 200);	
+		controller = new Controller();
+		tank = new Tank(controller, cvs);
+		window.requestAnimationFrame(function() {
+			tank.animate();
+		});
 	} else {
 		window.requestAnimationFrame(poll);
 	}
